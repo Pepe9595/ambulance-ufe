648cb6295f0ba1eb11ce75193386c764d05d9d56
Peter <pmacsik95@gmail.com>
2024-03-26 16:53:21 +0100

    one more renaming

--- src/components/pema-ambulance-wl-list/pema-ambulance-wl-list.tsx
+++ src/components/pema-ambulance-wl-list/pema-ambulance-wl-list.tsx
@@ -15,7 +15,7 @@ export class PemaAmbulanceWlList {
   private async getWaitingPatientsAsync() {
     return await Promise.resolve(
       [{
-        name: 'doc. Ing. Jožko Púčik, PhD.',
+        name: 'Peter Gabor',
         patientId: '10001',
         since: new Date(Date.now() - 10 * 60).toISOString(),
         estimatedStart: new Date(Date.now() + 65 * 60).toISOString(),
